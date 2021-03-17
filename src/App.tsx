8eb2a796a17d274fc49ef129589fe9212680492a
Alexandre Bitar <joabitar76@gmail.com>
2021-03-17 15:59:21 -0300

    chore: added component styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #7159c1;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>dtmoney</h1>
+      <Title>dtmoney</Title>
     </div>
   );
 }
