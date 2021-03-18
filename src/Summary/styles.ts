e3dfea6eca92fd241530f60ad61ed2ce4c9990b4
Alexandre Bitar <joabitar76@gmail.com>
2021-03-18 11:15:02 -0300

    style: added style from summary component

---
+++ src/Summary/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -4.5rem;
+
+  div {
+    background-color: var(--shape);
+    padding: 1.5rem 2rem;
+    color: var(--text-title);
+    border-radius: 0.25rem;
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    strong {
+      display: block;
+      font-size: 2rem;
+      margin-top: 1rem;
+      line-height: 3rem;
+      font-weight: 500;
+    }
+
+    &.highlight-background {
+      background-color: var(--green);
+      color: #fff;
+    }
+  }
+`;
