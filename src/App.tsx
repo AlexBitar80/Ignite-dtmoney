196883f3e8b740e1f942d4c3e25452bf502f9e3e
Alexandre Bitar <joabitar76@gmail.com>
2021-03-18 11:13:32 -0300

    chore: imported dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Dashboard } from './Dashboard';
 import { Header } from './Header';
 import { GlobalStyle } from './styles/global';
 
@@ -5,6 +6,7 @@ export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
