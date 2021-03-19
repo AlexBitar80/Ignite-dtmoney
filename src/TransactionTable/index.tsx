51b7af026c30f8ece97a2dd65bbff152900193ae
Alexandre Bitar <joabitar76@gmail.com>
2021-03-18 21:45:42 -0300

    chore: added use of api with mirageJS

--- src/TransactionTable/index.tsx
+++ src/TransactionTable/index.tsx
@@ -1,26 +1,41 @@
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 export function TransactionTable() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
+
   return (
     <Container>
       <table>
         <thead>
-          <th>Título</th>
-          <th>Preço</th>
-          <th>Categoria</th>
-          <th>Data</th>
+          <tr>
+            <th>Título</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
         </thead>
+
         <tbody>
-          <td>Desenvolvimento de site</td>
-          <td className="income">R$12.000,00</td>
-          <td>venda</td>
-          <td>18/03/2021</td>
+          <tr>
+            <td>Desenvolvimento de site</td>
+            <td className="income">R$12.000,00</td>
+            <td>venda</td>
+            <td>18/03/2021</td>
+          </tr>
         </tbody>
+
         <tbody>
-          <td>Supermercado</td>
-          <td className="outcome">- R$680,00</td>
-          <td>alimentação</td>
-          <td>13/03/2021</td>
+          <tr>
+            <td>Supermercado</td>
+            <td className="outcome">- R$680,00</td>
+            <td>alimentação</td>
+            <td>13/03/2021</td>
+          </tr>
         </tbody>
       </table>
     </Container>
