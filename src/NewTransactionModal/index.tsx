6d5cb8bf43246de9cfa26c3ffbc964208223d07c
Alexandre Bitar <joabitar76@gmail.com>
2021-03-19 15:56:33 -0300

    chore: added input and output button

--- src/NewTransactionModal/index.tsx
+++ src/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
+import incomeImg from '../assets/income.svg';
+import outcomeImg from '../assets/outcome.svg';
 import closeImg from '../assets/close.svg';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -32,6 +34,18 @@ export function NewTransactionModal({
         <input placeholder="Título" />
 
         <input placeholder="Valor" type="number" />
+
+        <TransactionTypeContainer>
+          <button>
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button>
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input placeholder="Categoria" />
 
