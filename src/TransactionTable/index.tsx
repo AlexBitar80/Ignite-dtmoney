6266d109f8f8d08e9014ed0e059a8d2c9a13ca3b
Alexandre Bitar <joabitar76@gmail.com>
2021-03-20 15:18:08 -0300

    chore: moved the logic to get the API data

--- src/TransactionTable/index.tsx
+++ src/TransactionTable/index.tsx
@@ -1,24 +1,9 @@
-import { useEffect, useState } from 'react';
-import { api } from '../services/api';
 import { Container } from './styles';
-
-interface Transactions {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createdAt: string;
-}
+import { TransactionsContext } from '../TransactionsContext';
+import { useContext } from 'react';
 
 export function TransactionTable() {
-  const [transactions, setTransactions] = useState<Transactions[]>([]);
-
-  useEffect(() => {
-    api
-      .get('transactions')
-      .then(response => setTransactions(response.data.transactions));
-  }, []);
+  const transactions = useContext(TransactionsContext);
 
   return (
     <Container>
