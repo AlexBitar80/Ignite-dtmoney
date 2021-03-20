515204122376814751bfc9ff251194c2c51ee116
Alexandre Bitar <joabitar76@gmail.com>
2021-03-20 00:16:58 -0300

    chore:  added color to the button based on the type

--- src/NewTransactionModal/index.tsx
+++ src/NewTransactionModal/index.tsx
@@ -2,7 +2,8 @@ import Modal from 'react-modal';
 import incomeImg from '../assets/income.svg';
 import outcomeImg from '../assets/outcome.svg';
 import closeImg from '../assets/close.svg';
-import { Container, TransactionTypeContainer } from './styles';
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
+import { useState } from 'react';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -13,6 +14,8 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       className="react-modal-content"
@@ -36,15 +39,27 @@ export function NewTransactionModal({
         <input placeholder="Valor" type="number" />
 
         <TransactionTypeContainer>
-          <button>
+          <RadioBox
+            type="button"
+            isActive={type === 'deposit'}
+            onClick={() => {
+              setType('deposit');
+            }}
+          >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
+          </RadioBox>
 
-          <button>
+          <RadioBox
+            type="button"
+            isActive={type === 'withdraw'}
+            onClick={() => {
+              setType('withdraw');
+            }}
+          >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
 
         <input placeholder="Categoria" />
